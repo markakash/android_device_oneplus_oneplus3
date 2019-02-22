b853872a592a2aab1dd7a763652d6f3619d85790
Akash <akashniki@gmail.com>
2019-02-22 20:55:37 +0000

    op3: Import hostapd configs from CAF https://source.codeaurora.org/quic/la/platform/vendor/qcom/wlan/tree/msmnile?h=LA.UM.7.3.r1-06700-sdm845.0

--- device.mk
+++ device.mk
@@ -283,6 +283,12 @@ PRODUCT_PACKAGES += \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0-java
 
+#Hotspot
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/hostapd.accept:system/etc/hostapd/hostapd.accept \
+    $(LOCAL_PATH)/configs/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf \
+    $(LOCAL_PATH)/configs/hostapd.deny:system/etc/hostapd/hostapd.deny
+
 # IPv6
 PRODUCT_PACKAGES += \
     ebtables \
